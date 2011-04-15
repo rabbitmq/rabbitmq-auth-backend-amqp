e042ed0f334667b32c0327f893873099bd0298ed
Simon MacMullen <simon@rabbitmq.com>
2011-04-15 15:55:44 +0100

    Slightly clearer.

--- examples/rabbitmq-auth-backend-java/src/com/rabbitmq/authbackend/examples/Main.java
+++ examples/rabbitmq-auth-backend-java/src/com/rabbitmq/authbackend/examples/Main.java
@@ -1,5 +1,6 @@
 package com.rabbitmq.authbackend.examples;
 
+import com.rabbitmq.authbackend.AuthBackend;
 import com.rabbitmq.authbackend.AuthServer;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -15,6 +16,7 @@ import java.util.Date;
 public class Main {
     private static final ConnectionFactory FACTORY = new ConnectionFactory();
     private static final String EXCHANGE = "authentication";
+    private static final AuthBackend BACKEND = new ExampleAuthBackend();
 
     public static void main(String[] args) throws IOException {
 
@@ -26,7 +28,7 @@ public class Main {
                     Channel ch = conn.createChannel();
                     System.out.println(" done");
 
-                    new AuthServer(new ExampleAuthBackend(), ch, EXCHANGE).mainloop();
+                    new AuthServer(BACKEND, ch, EXCHANGE).mainloop();
                     System.out.println(new Date() + " Connection died");
                 }
                 catch (ConnectException e) {
